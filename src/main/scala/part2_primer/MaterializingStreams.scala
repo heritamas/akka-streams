100949595d1cfaa6faabd256ddabefc9c0793327
Heri Tamas <heri.tamas@gmail.com>
2025-05-12 21:43:19 +0200

    delete dotfiles

--- src/main/scala/part2_primer/MaterializingStreams.scala
+++ src/main/scala/part2_primer/MaterializingStreams.scala
@@ -13,16 +13,16 @@ object MaterializingStreams extends App {
   // implicit val materializer: ActorMaterializer = ActorMaterializer()
   import system.dispatcher
 
-  val simpleGraph = Source(1 to 10).to(Sink.foreach(println))
-  //  val simpleMaterializedValue = simpleGraph.run()
+//  val simpleGraph = Source(1 to 10).to(Sink.foreach(println))
+//  val simpleMaterializedValue = simpleGraph.run()
 
   val source = Source(1 to 10)
   val sink = Sink.reduce[Int]((a, b) => a + b)
-  //  val sumFuture = source.runWith(sink)
-  //  sumFuture.onComplete {
-  //    case Success(value) => println(s"The sum of all elements is :$value")
-  //    case Failure(ex) => println(s"The sum of the elements could not be computed: $ex")
-  //  }
+  val sumFuture = source.runWith(sink)
+  sumFuture.onComplete {
+    case Success(value) => println(s"The sum of all elements is :$value")
+    case Failure(ex) => println(s"The sum of the elements could not be computed: $ex")
+  }
 
   // choosing materialized values
   val simpleSource = Source(1 to 10)
